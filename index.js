ccf364df9ce66fa3d912743362b8c35632324850
keroro0402 <otsuyu0402@yahoo.co.jp>
2024-08-03 17:25:32 +0900

    test7

--- index.js
+++ index.js
@@ -7,3 +7,46 @@ var string1 = 'こんばんは';
 console.log(string1);
 var string2 = 'おはようございます';
 console.log(string2);
+/** 数字型 */
+var num1 = 12;
+console.log(num1);
+var num2 = 10000;
+console.log(num2);
+/** obj */
+var obj1 = {
+    name: '斉藤ななみ',
+    age: 32,
+};
+console.log(obj1.name, obj1.age);
+var obj2 = {
+    food: {
+        name: 'ハンバーガー',
+        price: 500,
+        store: {
+            info1: '鎌倉点',
+            info2: '帯露店',
+        },
+    },
+    drink: {
+        name: 'コーラー',
+        price: 300,
+        essence: {
+            sugar: 40,
+            cal: 90,
+        },
+    },
+};
+console.log(obj2.food.name, obj2.drink.name, obj2.drink.essence);
+/** 真偽値 */
+var bool1 = false;
+if (bool1) {
+    console.log('間違い！！');
+}
+else {
+    console.log('正解〜〜！');
+}
+var bool2 = true;
+console.log(bool2 ? 'OK' : 'NG');
+var result1;
+result1 = bool1 || (bool2 && bool2) || bool1 || bool2 || bool1;
+console.log(result1);
--- index.ts
+++ index.ts
@@ -5,8 +5,76 @@
 /**TypeScriptの型付け */
 
 /**　文字列型 */
-
 let string1: string = 'こんばんは';
 console.log(string1);
 let string2: string = 'おはようございます';
 console.log(string2);
+
+/** 数字型 */
+let num1: number = 12;
+console.log(num1);
+let num2: number = 10000;
+console.log(num2);
+
+/** obj */
+let obj1: {
+  name: string;
+  age: number;
+} = {
+  name: '斉藤ななみ',
+  age: 32,
+};
+console.log(obj1.name, obj1.age);
+
+let obj2: {
+  food: {
+    name: string;
+    price: number;
+    store: {
+      info1: string;
+      info2: string;
+    };
+  };
+  drink: {
+    name: string;
+    price: number;
+    essence: {
+      sugar: number;
+      cal: number;
+    };
+  };
+} = {
+  food: {
+    name: 'ハンバーガー',
+    price: 500,
+    store: {
+      info1: '鎌倉点',
+      info2: '帯露店',
+    },
+  },
+  drink: {
+    name: 'コーラー',
+    price: 300,
+    essence: {
+      sugar: 40,
+      cal: 90,
+    },
+  },
+};
+
+console.log(obj2.food.name, obj2.drink.name, obj2.drink.essence);
+
+/** 真偽値 */
+let bool1: boolean = false;
+if (bool1) {
+  console.log('間違い！！');
+} else {
+  console.log('正解〜〜！');
+}
+
+let bool2: boolean = true;
+
+console.log(bool2 ? 'OK' : 'NG');
+let result1: boolean;
+result1 = bool1 || (bool2 && bool2) || bool1 || bool2 || bool1;
+console.log(result1);
